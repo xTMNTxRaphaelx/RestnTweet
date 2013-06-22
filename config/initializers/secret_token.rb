c70fcbac01116eeca9b8d51e9b9784456d9641ef
Rahul Garg <fructis_n3m0@live.in>
2013-06-22 22:37:49 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestnTweet::Application.config.secret_token = '716274afc78b5e1a469ebcc36774ef257ebd73c6bb85d54cbc75bcd8daae937f1c7cbba6c40d926a7c4294bb8f6f955bee8f7ad88a593e2c01939e24ab7426f7'
